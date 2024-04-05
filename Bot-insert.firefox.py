d90c029591b7819f783dea81858297b92037d516
Cristóbal Hernán Cornejo Valdebenito (cristobal.cornejo.v) <cristobal.cornejo.v@sc.uchile.dom>
2024-04-05 15:38:04 -0300

    Commit 1.2

--- Bot-insert.firefox.py
+++ Bot-insert.firefox.py
@@ -2,30 +2,32 @@ from selenium import webdriver
 from openpyxl import load_workbook
 import time
 
-# Variables genéricas
-RUTA_FIREFOX = r"RUTA_AL_EJECUTABLE_DE_FIREFOX"  # Cambia esta ruta por la ruta al ejecutable de Firefox en tu sistema
-RUTA_ARCHIVO_EXCEL = r"RUTA_AL_ARCHIVO_EXCEL"  # Cambia esta ruta por la ruta al archivo Excel en tu sistema
+# Rutas y archivos genéricos - Cambia estas rutas por las rutas específicas en tu sistema
+RUTA_FIREFOX = r"RUTA_AL_EJECUTABLE_DE_FIREFOX"
+RUTA_ARCHIVO_EXCEL = r"RUTA_AL_ARCHIVO_EXCEL"
 
 # Función para iniciar sesión en el sitio web - Cambia estas URL y credenciales por las de tu sitio
 def iniciar_sesion(driver):
-    driver.get("URL_DEL_SITIO_WEB")  # URL del sitio web
+    # Abrir la página de inicio de sesión
+    driver.get("URL_DEL_SITIO_WEB")
     time.sleep(2)
-    driver.find_element("name", "LOGIN").send_keys("USUARIO")  # Usuario
-    driver.find_element("name", "PASSWD").send_keys("CONTRASEÑA")  # Contraseña
+    # Introducir credenciales y hacer clic en el botón de inicio de sesión
+    driver.find_element("name", "LOGIN").send_keys("USUARIO")
+    driver.find_element("name", "PASSWD").send_keys("CONTRASEÑA")
     driver.find_element("name", "Valid_CNX").click()
     time.sleep(2)
 
 # Función para procesar los datos del archivo Excel y asignar etiquetas a los equipos
 def procesar_excel_y_asignar_etiquetas(driver, archivo_excel):
     wb = load_workbook(archivo_excel)
-    hoja_datos = wb['Hoja1']
+    hoja_nombres = wb['Hoja1']
     for i in range(2, 6):
-        nombre, apellido = hoja_datos[f'A{i}:B{i}'][0]
+        nombre, apellido = hoja_nombres[f'A{i}:B{i}'][0]
         print(nombre.value)
         print(apellido.value)
 
-        # Acceder a la URL del equipo específico - Cambia esta URL por la correspondiente a tu sitio
-        driver.get("URL_DEL_SITIO_WEB_PARA_EQUIPO" + str(nombre.value))  # URL del equipo específico
+        # Abrir la página de cada equipo y asignar etiqueta - Cambia esta URL por la correspondiente a tu sitio
+        driver.get("URL_DEL_SITIO_WEB_PARA_EQUIPO" + str(nombre.value))
         time.sleep(2)
         driver.find_element("name", "TAG").clear()
         time.sleep(2)
@@ -41,9 +43,9 @@ def main():
     opciones = webdriver.FirefoxOptions()
     opciones.binary_location = RUTA_FIREFOX
     driver = webdriver.Firefox(options=opciones)
-    iniciar_sesion(driver)
-    procesar_excel_y_asignar_etiquetas(driver, RUTA_ARCHIVO_EXCEL)
-    driver.quit()
+    iniciar_sesion(driver)  # Iniciar sesión en el sitio web
+    procesar_excel_y_asignar_etiquetas(driver, RUTA_ARCHIVO_EXCEL)  # Procesar Excel y asignar etiquetas
+    driver.quit()  # Cerrar el navegador al finalizar
 
 if __name__ == "__main__":
     main()
